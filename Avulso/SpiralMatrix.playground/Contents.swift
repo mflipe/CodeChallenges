6a0a7e237e8470ab4853da06e768938250c94387
Marcos Rocha <marcosrocha.dev@gmail.com>
2024-04-07 15:56:50 -0300

    Update Contents.swift

--- Avulso/SpiralMatrix.playground/Contents.swift
+++ Avulso/SpiralMatrix.playground/Contents.swift
@@ -30,52 +30,207 @@ import Foundation
 import XCTest
 
 // MARK: - Solution 1
+/* 1,630 s
 func solution(matrix: [[Int]]) -> [Int] {
+    guard !matrix.isEmpty else { return [] }
+    
     var result: [Int] = []
     
-    let firstColumn = getColumn(from: matrix, with: matrix.count, onColumn: 0)
-    result.append(contentsOf: firstColumn)
     
-    let lastRow = matrix.last!.dropFirst()
-    result.append(contentsOf: lastRow)
+    // Anti-clockwise
+    var currentFistRowIndex: Int = 0
+    var currentLastRowIndex: Int = matrix.count - 1
+    var rowSkipFirst: Int = 1
+    var rowSkipLast: Int = 0
     
-    let lastColumn = getColumn(from: matrix, with: matrix.count, onColumn: matrix[0].count - 1)
-        .reversed()
-        .dropFirst()
-    result.append(contentsOf: lastColumn)
+    var currentFirstColumnIndex: Int = 0
+    var currentLastColumnIndex: Int = matrix[0].count - 1
+    var columnSkipFirst: Int = 0
+    var columnSkipLast: Int = 0
     
-    let fistRow = matrix.first!
-        .reversed()
-        .dropFirst()
-        .dropLast()
-    result.append(contentsOf: fistRow)
+    var totalOfElements = matrix.count * matrix[0].count
     
-    let secondColumn = getColumn(from: matrix, with: matrix.count, onColumn: 1)
-        .dropFirst()
-        .dropLast()
-    result.append(contentsOf: secondColumn)
-    
-    let thirdColumn = getColumn(from: matrix, with: matrix.count, onColumn: 2)
-        .reversed()
-        .dropLast()
-        .dropFirst()
-    result.append(contentsOf: thirdColumn)
+    while(result.count < totalOfElements) {
+        
+        // Column 0
+        let firstColumn = getColumn(
+            from: matrix,
+            onColumn: currentFirstColumnIndex,
+            dropFirst: columnSkipFirst,
+            dropLast: columnSkipLast,
+            isReversed: false
+        )
+        result.append(contentsOf: firstColumn)
+        
+        currentFirstColumnIndex += 1
+        columnSkipFirst += 1
+        
+        if (result.count == totalOfElements) {
+            break
+        }
+        
+        // Row Last
+        let lastRow = getRow(
+            from: matrix,
+            onRow: currentLastRowIndex,
+            dropFirst: rowSkipFirst,
+            dropLast: rowSkipLast,
+            isReversed: false
+        )
+        result.append(contentsOf: lastRow)
+        
+        currentLastRowIndex -= 1
+        rowSkipLast += 1
+        
+        if (result.count == totalOfElements) {
+            break
+        }
+        
+        // Column Last
+        let lastColumn = getColumn(
+            from: matrix,
+            onColumn: currentLastColumnIndex,
+            dropFirst: columnSkipFirst,
+            dropLast: columnSkipLast,
+            isReversed: true
+        )
+        result.append(contentsOf: lastColumn)
+        
+        columnSkipLast += 1
+        
+        if (result.count == totalOfElements) {
+            break
+        }
+        
+        // Row 1
+        let fistRow = getRow(
+            from: matrix,
+            onRow: currentFistRowIndex,
+            dropFirst: rowSkipFirst,
+            dropLast: rowSkipLast,
+            isReversed: true
+        )
+        result.append(contentsOf: fistRow)
+        
+        currentFistRowIndex += 1
+        columnSkipLast += 1
+        
+        if (result.count == totalOfElements) {
+            break
+        }
+    }
     
     return result
 }
 
-func getColumn(from matrix: [[Int]], with numberOfRows: Int, onColumn index: Int) -> [Int]  {
+func getColumn(from matrix: [[Int]], onColumn index: Int,
+               dropFirst leftDrop: Int, dropLast rightDrop: Int, isReversed: Bool) -> [Int]  {
     var column: [Int] = []
+    let numberOfRows = matrix.count
     
     for i in stride(from: 0, to: numberOfRows, by: 1) {
         let currentValue = matrix[i][index]
         column.append(currentValue)
     }
     
+    column = applyDropAndInvert(from: column, dropFirst: leftDrop, dropLast: rightDrop, isReversed: isReversed)
+    
     return column
 }
 
+func getRow(from matrix: [[Int]], onRow index: Int,
+            dropFirst leftDrop: Int, dropLast rightDrop: Int, isReversed: Bool) -> [Int] {
+    
+    var row: [Int] = matrix[index]
+
+    row = applyDropAndInvert(from: row, dropFirst: leftDrop, dropLast: rightDrop, isReversed: isReversed)
+
+    return row
+}
+
+func applyDropAndInvert(from array: [Int],
+            dropFirst leftDrop: Int, dropLast rightDrop: Int, isReversed: Bool) -> [Int] {
+    var newArray = array
+    
+    if (isReversed) {
+        newArray = Array(newArray.reversed())
+    }
+    
+    if (leftDrop > 0) {
+        newArray = Array(newArray.dropFirst(leftDrop))
+    }
+    
+    if (rightDrop > 0 ) {
+        newArray = Array(newArray.dropLast(rightDrop))
+    }
+    
+    return newArray
+}
+*/
 // MARK: - Solution 2
+func solution(matrix arr: [[Int]]) -> [Int] {
+    guard !arr.isEmpty else { return [] }
+    
+    var m = arr.count
+    var n = arr[0].count
+    
+    var result: [Int] = []
+    
+    var k = 0, l = 0
+    var cnt = 0
+    let total = m * n
+    
+    while k < m && l < n {
+        if cnt == total {
+            break
+        }
+        
+        // Print the first column from the remaining columns
+        for i in k..<m {
+            result.append(arr[i][l])
+            cnt += 1
+        }
+        l += 1
+        
+        if cnt == total {
+            break
+        }
+        
+        // Print the last row from the remaining rows
+        for i in l..<n {
+            result.append(arr[m - 1][i])
+            cnt += 1
+        }
+        m -= 1
+        
+        if cnt == total {
+            break
+        }
+        
+        // Print the last column from the remaining columns
+        if k < m {
+            for i in (k..<m).reversed() {
+                result.append(arr[i][n - 1])
+                cnt += 1
+            }
+            n -= 1
+        }
+        
+        if cnt == total {
+            break
+        }
+        
+        // Print the first row from the remaining rows
+        if l < n {
+            for i in (l..<n).reversed() {
+                result.append(arr[k][i])
+                cnt += 1
+            }
+            k += 1
+        }
+    }
+    return result
+}
 
 // MARK: - Test Cases
 class Tests: XCTestCase {
@@ -106,7 +261,7 @@ class Tests: XCTestCase {
         }
     }
 
-    func test1() {
+    func test0() {
         let input: [[Int]] = [[ 1, 2, 3, 4],
                               [ 5, 6, 7, 8],
                               [ 9, 10, 11, 12],
@@ -122,11 +277,65 @@ class Tests: XCTestCase {
         XCTAssertEqual(result, output)
     }
     
-    func test2() {
+    func test1() {
         let input: [[Int]] = [[ 1, 2, 3, 4, 5, 6],
                               [ 7, 8, 9, 10, 11, 12],
                               [ 13, 14, 15, 16, 17, 18]]
         let output: [Int] = [1, 7, 13, 14, 15, 16, 17, 18, 12, 6, 5, 4, 3, 2, 8, 9, 10, 11]
+        
+        var result: [Int] = []
+        
+        measure {
+            result = solution(matrix: input)
+        }
+        
+        XCTAssertEqual(result, output)
+    }
+    func test2() {
+        let input: [[Int]] = [[1, 2, 3],
+                              [4, 5, 6],
+                              [7, 8, 9]]
+        let output: [Int] = [1, 4, 7, 8, 9, 6, 3, 2, 5]
+        
+        var result: [Int] = []
+        
+        measure {
+            result = solution(matrix: input)
+        }
+        
+        XCTAssertEqual(result, output)
+    }
+    
+    func test3() {
+        let input: [[Int]] = [[1, 2],
+                              [3, 4]]
+        let output: [Int] = [1, 3, 4, 2]
+        
+        var result: [Int] = []
+        
+        measure {
+            result = solution(matrix: input)
+        }
+        
+        XCTAssertEqual(result, output)
+    }
+    
+    func test4() {
+        let input: [[Int]] = [[1]]
+        let output: [Int] = [1]
+        
+        var result: [Int] = []
+        
+        measure {
+            result = solution(matrix: input)
+        }
+        
+        XCTAssertEqual(result, output)
+    }
+    
+    func test5() {
+        let input: [[Int]] = []
+        let output: [Int] = []
         
         var result: [Int] = []
         
